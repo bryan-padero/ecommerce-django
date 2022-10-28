5e6d7a161d7425d11beef6a51fdc63d408f9f23d
Bryan <bryanpadero@gmail.com>
2022-10-28 14:45:56 +0400

    added login.html and functionality, login activation using token and email and added dashboard.html

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -121,6 +121,20 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+
+# SMTP configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'amazingbryan02@gmail.com'
+EMAIL_HOST_PASSWORD = 'gjuyrfuuldszgmih'
+EMAIL_USE_TLS = True
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
