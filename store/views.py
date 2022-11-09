dbbf54b26495d6ae7d44fab22d1bd8c4ea9fb5b2
Bryan <bryanpadero@gmail.com>
2022-11-09 22:47:27 +0400

    fixed product gallery

--- store/views.py
+++ store/views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Product, ReviewRating
+from .models import Product, ReviewRating, ProductGallery
 from .forms import ReviewForm
 from carts.models import CartItem
 from carts.views import _cart_id
@@ -47,7 +47,12 @@ def product_detail(request, category_slug, product_slug):
     # Get reviews
     reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
 
+    # Get product gallery
+
+    product_gallery = ProductGallery.objects.filter(product_id=single_product.id)
+
     context = {
+        'product_gallery': product_gallery,
         'reviews': reviews,
         'single_product': single_product,
         'in_cart': in_cart,
